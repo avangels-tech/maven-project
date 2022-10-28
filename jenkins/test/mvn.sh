1d59642f8ae41060362ea014faeb36c55be2955c
avangels-tech <115863890+avangels-tech@users.noreply.github.com>
2022-10-28 19:50:10 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -8,6 +8,10 @@ echo "********************"
 
 #WORKSPACE=/var/efs/podtest-data/jenkins_entp/jenkins/workspace/avangels1-pipeline-maven
 
+WORKSPACE=/home/prok8adm/jenkins/jenkins/workspace/pipeline-docker-maven/java-app
+
+docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11  sh -c 'whoami > userFile'"$@"
+
 #docker run --rm -v $PWD/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11  sh -c 'whoami > userFile'"$@"
 
-docker run --rm -v  $PWD/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11 "$@"
+#docker run --rm -v  $PWD/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11 "$@"
